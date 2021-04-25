71003eb19590bfc6beb6f333288e3960860e3929
Gustavo <gustavo_dmuniz@hotmail.com>
2021-04-25 20:23:05 -0300

    [TAPP6] add lib to binding request params

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_request_params import bind_request_params
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+app.before_request(bind_request_params)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
