e19880b3413fb36b7150d057be73e87f3107e98a
Gustavo <gustavo_dmuniz@hotmail.com>
2021-04-26 00:00:24 -0300

    [TAPP6] remove base model comments

--- app/models/base.py
+++ app/models/base.py
@@ -3,13 +3,6 @@ from flask_sqlalchemy import inspect
 
 class Base(db.Model):
   __abstract__ = True
-
-  # def attributes():
-  #   for attr in inspect.getmembers(User):
-  #       if not attr[0].startswith('_'):
-  #         if not inspect.ismethod(attr[1]):
-  #           print(attr)
-    # if not i[0].startswith('_'):
   
   def save(self):
     try:
