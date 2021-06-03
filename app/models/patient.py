ded11a12f39ab6bbf3bf843df767ecc1b4466a73
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-06-03 20:04:29 -0300

    [TAPP] Add patient model

---
+++ app/models/patient.py
@@ -0,0 +1,16 @@
+from app.models.base import Base
+from app import bcrypt
+from app import db 
+
+class Patient(Base):
+  __tablename__ = "patients"
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  email = db.Column(db.String, nullable=False)
+  phone = db.Column(db.String, nullable=False)
+
+  def __init__(self, name, email, phone):
+    self.name = name
+    self.email = email
+    self.phone = phone
