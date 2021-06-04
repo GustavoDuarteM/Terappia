dacb039bb3dc68337799396724f776f3a6a1a63d
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-06-04 01:15:47 -0300

    [TAPP8] Add update session

--- app/controllers/sessions_controller.py
+++ app/controllers/sessions_controller.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify
-from app import app
+from app import app , datetime
 from app.models.session import Session
 
 @app.route('/sessions', methods=['POST'])
@@ -19,5 +19,16 @@ def show_sessions(id):
   else:
     return jsonify({"status": "Sessão inválida"}), 404
 
+@app.route('/sessions', methods=['PUT'])
+def edit_sessions():
+  params = session_params()
+  session = Session.query.get(params['id'])
+  session.start =  datetime.strptime(params['start'], "%Y-%m-%d %H:%M")
+  session.end = datetime.strptime(params['end'], "%Y-%m-%d %H:%M") 
+  if session.save():
+      return jsonify(session.serialize(['user','patient']))
+  else:
+    return jsonify({"status": "Sessão inválida"}), 404
+
 def session_params():
-  return request.params.require('session').permit("start", "end", "patient_id", "user_id")
+  return request.params.require('session').permit("id","start", "end", "patient_id", "user_id")
