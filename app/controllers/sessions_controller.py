c22e66c6109affb1283597699a36b337fc34bc44
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-06-04 01:03:14 -0300

    [TAPP8] Add endpoint new session

---
+++ app/controllers/sessions_controller.py
@@ -0,0 +1,16 @@
+from flask import request, jsonify
+from app import app
+from app.models.session import Session
+
+@app.route('/sessions', methods=['POST'])
+def new_sessions():
+  params = session_params()
+  session = Session(**session_params())
+  if session.save():
+      return jsonify(session.serialize(['user','patient']))
+  else:
+    return jsonify({"status": "Paciênte inválido"}), 404
+
+
+def session_params():
+  return request.params.require('session').permit("start", "end", "patient_id", "user_id")
