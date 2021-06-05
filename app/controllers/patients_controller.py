bc4e61aad7aeb0b87ae94489962db223cd7cc8ac
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-06-04 23:27:53 -0300

    [TAPP] Fix load patient

--- app/controllers/patients_controller.py
+++ app/controllers/patients_controller.py
@@ -1,23 +1,26 @@
+from app.models import user
 from flask import request, jsonify
 from app import app
 from app.models.patient import Patient
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, current_user
 
 @app.route('/patients', methods=['POST'])
 @jwt_required()
 def new_patients():
-  patient = Patient(**patient_params())
+  patient = Patient(**patient_params(), user_id = current_user.id )
   if patient.save():
-      return jsonify(patient.serialize(['user']))
+      return jsonify(patient = patient.serialize(['user','sessions']))
   else:
     return jsonify({"status": "Paciênte inválido"}), 404
 
 @app.route('/patients/<int:id>', methods=['GET'])
 @jwt_required()
 def show_patients(id):
-  patient = Patient.query.get(id)
+  print(id)
+  print(current_user.id)
+  patient = Patient.query.filter_by(id = id, user_id = current_user.id).first()
   if patient:
-      return jsonify(patient.serialize(['user']))
+      return jsonify(patient = patient.serialize(['user','sessions']))
   else:
     return jsonify({"status": "Paciente inválido"}), 404
 
@@ -25,12 +28,12 @@ def show_patients(id):
 @jwt_required()
 def edit_patients():
   params = patient_params()
-  patient = Patient.query.get(params['id'])
+  patient = Patient.query.filter_by(id= params['id'], user_id= current_user.id).first()
   patient.setattrs(**patient_params())
   if patient.save():
-      return jsonify(patient.serialize(['user']))
+    return jsonify(patient = patient.serialize(['user','sessions']))
   else:
     return jsonify({"status": "Paciênte inválido"}), 404
 
 def patient_params():
-  return request.params.require('patient').permit("id","name", "email", "phone", "user_id")
+  return request.params.require('patient').permit("id","name", "email", "phone")
