fdca0361330ba6f498cd4fb6403ffa4e625491af
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-06-08 20:43:36 -0300

    change

--- application/__init__.py
+++ application/__init__.py
@@ -26,7 +26,7 @@ app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 jwt = JWTManager(app)
 jwt_redis_blocklist = redis.StrictRedis(
-  host="redis://:pd836355fd7e03187b62a458944ebc079be3d250642ebcd439422371994a18d5e@ec2-3-217-135-220.compute-1.amazonaws.com", 
+  host="pd836355fd7e03187b62a458944ebc079be3d250642ebcd439422371994a18d5e@ec2-3-217-135-220.compute-1.amazonaws.com", 
   port=24249,
    db=0, 
    decode_responses=True
