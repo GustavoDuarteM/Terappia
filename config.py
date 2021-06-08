af8c82dcefa440de3cf30be00bab2c3e7506d644
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-06-08 20:31:10 -0300

    change

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@db:5432/terapia_app"
+SQLALCHEMY_DATABASE_URI = "postgres://eexwdjkdwrilri:2856cba85feb5a17ca01fd5949f31ea225af64e75bc39f2c95027274a2951bf2@ec2-3-233-7-12.compute-1.amazonaws.com:5432/dd9cgagtik9jqm"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 JWT_SECRET_KEY = "super-secret" 
