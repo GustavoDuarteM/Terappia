b4fcf7f918568612a88698c518ed59637ae04a8a
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-06-08 23:34:44 -0300

    Adjust file config

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ import re
 SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL').replace("postgres","postgresql")
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY')
-REDIS_HOST = environ.get('REDIS_HOST')
-REDIS_PORT = environ.get('REDIS_PORT')
-REDIS_PASSWORD = environ.get('REDIS_PASSWORD')
-
-if environ.get('REDIS_URL'):
+if 'REDIS_URL' in environ:
   REDIS_HOST = re.split('[\: :@]',environ.get('REDIS_URL'))[3]
   REDIS_PASSWORD = re.split('[\: :@]',environ.get('REDIS_URL'))[2]
   REDIS_PORT = re.split('[\: :@]',environ.get('REDIS_URL'))[-1]
+else:
+  REDIS_HOST = environ.get('REDIS_HOST')
+  REDIS_PORT = environ.get('REDIS_PORT')
+  REDIS_PASSWORD = environ.get('REDIS_PASSWORD')
